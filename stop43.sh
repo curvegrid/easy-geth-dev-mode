4c04e1809067caab7791e249ed2c7d4abbac5d32
Jeff Wentworth <jeff@curvegrid.com>
2017-08-24 23:38:44 +0900

    use SIGHUP instead of SIGINT

--- stop43.sh
+++ stop43.sh
@@ -6,6 +6,6 @@ GETH_TESTNET_LOGDIR=${GETH_TESTNET_CHAINDIR}/log
 
 if [ -e ${GETH_TESTNET_LOGDIR}/geth.pid ]
 then
-	kill -INT $(cat ${GETH_TESTNET_LOGDIR}/geth.pid)
+	kill -HUP $(cat ${GETH_TESTNET_LOGDIR}/geth.pid)
 	rm ${GETH_TESTNET_LOGDIR}/geth.pid
 fi
