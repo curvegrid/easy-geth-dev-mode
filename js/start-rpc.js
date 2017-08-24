160fef220ec9989df4bd35b725dcdd536d135d81
Jeff Wentworth <jeff@curvegrid.com>
2017-08-24 12:27:01 +0900

    accept RPC connections on any localhost ports

--- js/start-rpc.js
+++ js/start-rpc.js
@@ -1,5 +1,5 @@
 try {
-  admin.startRPC('localhost', 8545, 'http://localhost:8080', 'web3,eth,personal,net');
+  admin.startRPC('localhost', 8545, 'http://localhost:*', 'web3,eth,personal,net');
 } catch(err) {
   console.log('Could not start RPC');
   console.log(err);
