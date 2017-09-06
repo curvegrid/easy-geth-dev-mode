7679b80775d5524c4a7bd170e5469e742baf07bf
Jeff Wentworth <jeff@curvegrid.com>
2017-09-06 17:30:23 +0900

    add debug to rpcapi

--- js/start-rpc.js
+++ js/start-rpc.js
@@ -1,5 +1,5 @@
 try {
-  admin.startRPC('localhost', 8545, 'http://localhost:*', 'web3,eth,personal,net');
+  admin.startRPC('localhost', 8545, 'http://localhost:*', 'web3,eth,personal,net,debug');
 } catch(err) {
   console.log('Could not start RPC');
   console.log(err);
