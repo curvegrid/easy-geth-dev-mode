b4870274d5489dbd5f69042b20efba2fa16a7509
Jeff Wentworth <jeff@curvegrid.com>
2017-12-20 13:57:34 +0900

    add 'admin' to exported RPC interfaces

--- js/start-rpc.js
+++ js/start-rpc.js
@@ -1,5 +1,5 @@
 try {
-  admin.startRPC('localhost', 8545, 'http://localhost:*', 'web3,eth,personal,net,debug');
+  admin.startRPC('localhost', 8545, 'http://localhost:*', 'web3,eth,personal,net,debug,admin');
 } catch(err) {
   console.log('Could not start RPC');
   console.log(err);
